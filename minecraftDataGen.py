c6f861b2b3aecf1750335b153ad0873a03c1b8ea
Strubbelkopp <nils130902@gmail.com>
2022-05-31 00:54:47 +0200

    Made it easier to create blocks with a different texture than their block id

--- minecraftDataGen.py
+++ minecraftDataGen.py
@@ -1,26 +1,6 @@
 import json
 import os
 
-#potentially change texture name for stais, slabs, fences, etc.
-#eg.: "texture": "baobab" -> "texture": "baobab_planks"
-
-#change double slab model in slab blockstate files
-#eg.: "model": "limestone_slab" -> "model": "limestone"
-
-#---<names in genData.json>---
-#"block": "NAME"
-#"stairs": "NAME_stairs"
-#"slab": "NAME_slab"
-#"button": "NAME_button"
-#"door": "NAME_door"
-#"pressure_plate": "NAME_pressure_plate"
-#"fence": "NAME_fence"
-#"fence_gate": "NAME_fence_gate"
-#"pillar_block": "NAME"
-#"sign": "NAME_sign"
-#"trapdoor": "NAME_trapdoor"
-#"wall": "NAME_wall"
-
 def createDirectories():
     if not os.path.exists(getFilePath('output/blockstates')):
         os.makedirs(getFilePath('output/blockstates'))
@@ -49,10 +29,16 @@ def replaceData(mod_id, name, data):
     data = data.replace("NAME", name)
     return data
 
-def saveNewFile(mod_id, replace_name, template_path, output_path):
+def saveNewFile(mod_id, name, template_path, output_path):
     data = readFile(template_path)
-    data = replaceData(mod_id, replace_name, data)
-    writeFile((output_path), data)
+    data = replaceData(mod_id, name, data)
+    writeFile(output_path, data)
+
+def saveSlabBlockstate(mod_id, name1, name2, template_path, output_path):
+    data = readFile(template_path)
+    data = data.replace("BLOCK_MODEL_NAME", name2)
+    data = replaceData(mod_id, name1, data)
+    writeFile(output_path, data)
 
 def createBlockAssets(mod_id, name):
     #model
@@ -64,16 +50,13 @@ def createBlockAssets(mod_id, name):
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropSelfTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createStairAssets(mod_id, name):
-    truncated_name = name.replace("_stairs", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
+def createStairAssets(mod_id, name, texture):
     #model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/stairsTemplate.json", "output/models/block/" + name + ".json")
+    saveNewFile(mod_id, texture, "templates/models/block/stairsTemplate.json", "output/models/block/" + name + ".json")
     #inner model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/stairsInnerTemplate.json", "output/models/block/" + name + "_inner.json")
+    saveNewFile(mod_id, texture, "templates/models/block/stairsInnerTemplate.json", "output/models/block/" + name + "_inner.json")
     #outer model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/stairsOuterTemplate.json", "output/models/block/" + name + "_outer.json")
+    saveNewFile(mod_id, texture, "templates/models/block/stairsOuterTemplate.json", "output/models/block/" + name + "_outer.json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/blockItemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
@@ -81,31 +64,26 @@ def createStairAssets(mod_id, name):
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropSelfTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createSlabAssets(mod_id, name):
-    truncated_name = name.replace("_slab", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
+def createSlabAssets(mod_id, name, texture, ):
+    truncated_name = name.replace("_sign", '')
     #slab model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/slabTemplate.json", "output/models/block/" + name + ".json")
+    saveNewFile(mod_id, texture, "templates/models/block/slabTemplate.json", "output/models/block/" + name + ".json")
     #slab top model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/slabTopTemplate.json", "output/models/block/" + name + "_top.json")
+    saveNewFile(mod_id, texture, "templates/models/block/slabTopTemplate.json", "output/models/block/" + name + "_top.json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/blockItemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
-    saveNewFile(mod_id, truncated_name, "templates/blockstates/slabTemplate.json", "output/blockstates/" + name + ".json")
+    saveSlabBlockstate(mod_id, name, texture, "templates/blockstates/slabTemplate.json", "output/blockstates/" + name + ".json")
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropSlabTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createButtonAssets(mod_id, name):
-    truncated_name = name.replace("_button", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
+def createButtonAssets(mod_id, name, texture):
     #pressed model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/buttonPressedTemplate.json", "output/models/block/" + name + "_pressed.json")
+    saveNewFile(mod_id, texture, "templates/models/block/buttonPressedTemplate.json", "output/models/block/" + name + "_pressed.json")
     #button model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/buttonTemplate.json", "output/models/block/" + name + ".json")
+    saveNewFile(mod_id, texture, "templates/models/block/buttonTemplate.json", "output/models/block/" + name + ".json")
     #inventory model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/buttonInventoryTemplate.json", "output/models/block/" + name + "_inventory.json")
+    saveNewFile(mod_id, texture, "templates/models/block/buttonInventoryTemplate.json", "output/models/block/" + name + "_inventory.json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/inventoryItemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
@@ -129,14 +107,11 @@ def createDoorAssets(mod_id, name):
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropDoorTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createPressurePlateAssets(mod_id, name):
-    truncated_name = name.replace("_pressure_plate", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
+def createPressurePlateAssets(mod_id, name, texture):
     #pressure plate model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/pressurePlateTemplate.json", "output/models/block/" + name + ".json")
+    saveNewFile(mod_id, texture, "templates/models/block/pressurePlateTemplate.json", "output/models/block/" + name + ".json")
     #pressure plate down model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/pressurePlateDownTemplate.json", "output/models/block/" + name + "_down.json")
+    saveNewFile(mod_id, texture, "templates/models/block/pressurePlateDownTemplate.json", "output/models/block/" + name + "_down.json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/blockItemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
@@ -144,16 +119,13 @@ def createPressurePlateAssets(mod_id, name):
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropSelfTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createFenceAssets(mod_id, name):
-    truncated_name = name.replace("_fence", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
+def createFenceAssets(mod_id, name, texture):
     #fence side model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/fenceSideTemplate.json", "output/models/block/" + name + "_side.json")
+    saveNewFile(mod_id, texture, "templates/models/block/fenceSideTemplate.json", "output/models/block/" + name + "_side.json")
     #fence post model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/fencePostTemplate.json", "output/models/block/" + name + "_post.json")
+    saveNewFile(mod_id, texture, "templates/models/block/fencePostTemplate.json", "output/models/block/" + name + "_post.json")
     #inventory model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/fenceInventoryTemplate.json", "output/models/block/" + name + "_inventory.json")
+    saveNewFile(mod_id, texture, "templates/models/block/fenceInventoryTemplate.json", "output/models/block/" + name + "_inventory.json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/inventoryItemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
@@ -161,18 +133,15 @@ def createFenceAssets(mod_id, name):
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropSelfTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createFenceGateAssets(mod_id, name):
-    truncated_name = name.replace("_fence_gate", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
+def createFenceGateAssets(mod_id, name, texture):
     #fence gate model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/fenceGateTemplate.json", "output/models/block/" + name + ".json")
+    saveNewFile(mod_id, texture, "templates/models/block/fenceGateTemplate.json", "output/models/block/" + name + ".json")
     #fence gate open model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/fenceGateOpenTemplate.json", "output/models/block/" + name + "_open.json")
+    saveNewFile(mod_id, texture, "templates/models/block/fenceGateOpenTemplate.json", "output/models/block/" + name + "_open.json")
     #fence gate wall model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/fenceGateWallTemplate.json", "output/models/block/" + name + "_wall.json")
+    saveNewFile(mod_id, texture, "templates/models/block/fenceGateWallTemplate.json", "output/models/block/" + name + "_wall.json")
     #fence gate wall open model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/fenceGateWallOpenTemplate.json", "output/models/block/" + name + "_wall_open.json")
+    saveNewFile(mod_id, texture, "templates/models/block/fenceGateWallOpenTemplate.json", "output/models/block/" + name + "_wall_open.json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/blockItemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
@@ -200,12 +169,10 @@ def createPillarBlockAssets(mod_id, name):
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropSelfTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createSignAssets(mod_id, name):
+def createSignAssets(mod_id, name, particle_texture):
     truncated_name = name.replace("_sign", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
     #sign model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/signTemplate.json", "output/models/block/" + name + ".json")
+    saveNewFile(mod_id, particle_texture, "templates/models/block/signTemplate.json", "output/models/block/" + name + ".json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/itemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
@@ -229,18 +196,15 @@ def createTrapdoorAssets(mod_id, name):
     #loot_table
     saveNewFile(mod_id, name, "templates/loot_tables/blocks/dropSelfTemplate.json", "output/loot_tables/blocks/" + name + ".json")
 
-def createWallAssets(mod_id, name):
-    truncated_name = name.replace("_wall", '')
-    if("brick" in truncated_name):
-        truncated_name = truncated_name.replace("brick", "bricks")
+def createWallAssets(mod_id, name, texture):
     #wall side model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/wallSideTemplate.json", "output/models/block/" + name + "_side.json")
+    saveNewFile(mod_id, texture, "templates/models/block/wallSideTemplate.json", "output/models/block/" + name + "_side.json")
     #wall side tall model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/wallSideTallTemplate.json", "output/models/block/" + name + "_side_tall.json")
+    saveNewFile(mod_id, texture, "templates/models/block/wallSideTallTemplate.json", "output/models/block/" + name + "_side_tall.json")
     #wall post model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/wallPostTemplate.json", "output/models/block/" + name + "_post.json")
+    saveNewFile(mod_id, texture, "templates/models/block/wallPostTemplate.json", "output/models/block/" + name + "_post.json")
     #inventory model
-    saveNewFile(mod_id, truncated_name, "templates/models/block/wallInventoryTemplate.json", "output/models/block/" + name + "_inventory.json")
+    saveNewFile(mod_id, texture, "templates/models/block/wallInventoryTemplate.json", "output/models/block/" + name + "_inventory.json")
     #item model
     saveNewFile(mod_id, name, "templates/models/item/inventoryItemTemplate.json", "output/models/item/" + name + ".json")
     #blockstate
@@ -273,36 +237,36 @@ def createTallPlantAssets(mod_id, name):
 def createAssets():
     data = json.loads(readFile("genData.json"))
     mod_id = data["mod_id"]
-    for name in data["block"]:
-        createBlockAssets(mod_id, name)
-    for name in data["stairs"]:
-        createStairAssets(mod_id, name)
-    for name in data["slab"]:
-        createSlabAssets(mod_id, name)
-    for name in data["button"]:
-        createButtonAssets(mod_id, name)
-    for name in data["door"]:
-        createDoorAssets(mod_id, name)
-    for name in data["pressure_plate"]:
-        createPressurePlateAssets(mod_id, name)
-    for name in data["fence"]:
-        createFenceAssets(mod_id, name)
-    for name in data["fence_gate"]:
-        createFenceGateAssets(mod_id, name)
-    for name in data["orientable_pillar_block"]:
-        createOrientablePillarBlockAssets(mod_id, name)
-    for name in data["pillar_block"]:
-        createPillarBlockAssets(mod_id, name)
-    for name in data["sign"]:
-        createSignAssets(mod_id, name)
-    for name in data["trapdoor"]:
-        createTrapdoorAssets(mod_id, name)
-    for name in data["wall"]:
-        createWallAssets(mod_id, name)
-    for name in data["plant"]:
-        createPlantAssets(mod_id, name)
-    for name in data["tall_plant"]:
-        createTallPlantAssets(mod_id, name)
+    for block in data["block"]:
+        createBlockAssets(mod_id, block)
+    for stairs in data["stairs"]:
+        createStairAssets(mod_id, stairs[0], stairs[1])
+    for slab in data["slab"]:
+        createSlabAssets(mod_id, slab[0], slab[1])
+    for button in data["button"]:
+        createButtonAssets(mod_id, button[0], button[1])
+    for door in data["door"]:
+        createDoorAssets(mod_id, door)
+    for pressure_plate in data["pressure_plate"]:
+        createPressurePlateAssets(mod_id, pressure_plate[0], pressure_plate[1])
+    for fence in data["fence"]:
+        createFenceAssets(mod_id, fence[0], fence[1])
+    for fence_gate in data["fence_gate"]:
+        createFenceGateAssets(mod_id, fence_gate[0], fence_gate[1])
+    for orientable_pillar_block in data["orientable_pillar_block"]:
+        createOrientablePillarBlockAssets(mod_id, orientable_pillar_block)
+    for pillar_block in data["pillar_block"]:
+        createPillarBlockAssets(mod_id, pillar_block)
+    for sign in data["sign"]:
+        createSignAssets(mod_id, sign[0], sign[1])
+    for trapdoor in data["trapdoor"]:
+        createTrapdoorAssets(mod_id, trapdoor)
+    for wall in data["wall"]:
+        createWallAssets(mod_id, wall[0], wall[1])
+    for plant in data["plant"]:
+        createPlantAssets(mod_id, plant)
+    for tall_plant in data["tall_plant"]:
+        createTallPlantAssets(mod_id, tall_plant)
 
 createDirectories()
 createAssets()
